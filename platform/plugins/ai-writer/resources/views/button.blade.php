58e511f1a2b118873d1dd35eb19830538f2129f7
mukatay <malikmukatay@gmail.com>
2023-05-25 18:28:56 +0600

    Add existing file

---
+++ platform/plugins/ai-writer/resources/views/button.blade.php
@@ -0,0 +1,11 @@
+<ul class="list-shortcode-items">
+    <li>
+        <span class="editor-action-item">
+            <a href="{{ route('short-codes.ajax-get-admin-config', 'ai-writer') }}"
+               data-has-admin-config="1" data-key="ai-writer" data-key="ai-writer" data-description="AI writer"
+               class="btn btn-info add_shortcode_btn_trigger">
+                <i class="fas fa-pen"></i> {{ trans('plugins/ai-writer::settings.title') }}
+            </a>
+        </span>
+    </li>
+</ul>
